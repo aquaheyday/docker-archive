b000fa7002e0668e1cd167a087ca980ae6fa7eb8
ByoungJun Kim <aquaheyday@gmail.com>
2025-01-23 09:41:27 +0900

    Update main.go

--- blue-green-deployment/app/main.go
+++ blue-green-deployment/app/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello, Go Modules!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, Go Modules!")
+	})
+
+	fmt.Println("Starting server on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
